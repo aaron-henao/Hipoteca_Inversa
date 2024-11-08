5a62189bdd60a91427fc54e171151f40c4d65cdf
Aaron. <110606543+aaron-henao@users.noreply.github.com>
2024-11-08 05:37:58 -0500

    Create Secret_Config-sample.py

---
+++ src/app/Secret_Config-sample.py
@@ -0,0 +1,14 @@
+"""
+
+Datos secretos que no deben publicarse en el repo
+
+Diligencie estos datos y guarde este archivo como Secret_Config.py
+para poder ejecutar la aplicación
+
+"""
+
+PGDATABASE = "DATABASE NAME"
+PGUSER = "USER"
+PGPASSWORD = "PASSWORD"
+PGHOST = "HOST"
+PGPORT = 5432 # POR DEFECTO ES 5432, PERO PUEDE CAMBIAR EN SU DB
